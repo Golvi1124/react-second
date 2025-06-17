b90259c788693add25807c1e2b54514cb893dfce
Golvi1124 <golvi1124@gmail.com>
2025-06-17 12:23:11 +0200

    Cookie tab done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ export default function Header() {
       <h1>Welcome to the second task of React.</h1>
       <p>* Made by using useState() and useEffect()</p>
       <h3>Here you can change to your preferred theme:</h3>
-
     </header>
   );
 }
--- src/tasks/CookieClicker.jsx
+++ src/tasks/CookieClicker.jsx
@@ -1,21 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function CookieClicker() {
   const [count, setCount] = useState(0);
   const handleClick = () => setCount((c) => c + 1);
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: "center" }}>
       <h2>Cookies: {count}</h2>
+      <p>Click on the cookie to increase the count!</p>
       <img
         src="https://png.pngtree.com/png-vector/20241022/ourlarge/pngtree-whimsical-cookie-clipart-for-invitations-and-more-png-image_14113699.png"
         alt="cookie"
         onClick={handleClick}
-        style={{ cursor: 'pointer', width: '200px' }}
+        style={{ cursor: "pointer", width: "200px" }}
       />
+
       <div>
-        <button onClick={() => setCount(0)} style={{ marginTop: '0.5rem' }}>
-          Reset
+        <button onClick={toggleVisibility}>
+          {isVisible ? "Hide Option" : "Want to erase progress?"}
         </button>
+
+        {isVisible && (
+          <>
+            <p>Click on button below and start new!</p>
+            <button onClick={() => setCount(0)} style={{ marginTop: "0.5rem" }}>
+              Reset
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
